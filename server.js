260f152657661db2a67c5f03e6e7904ae4cdee8a
microPchel <ktotutakoy18@mail.ru>
2025-05-22 12:49:59 +0300

    Описание изменений

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:4000',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST']
   }
 });
